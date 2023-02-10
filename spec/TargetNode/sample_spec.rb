93a8812af416a99579654119ea5e540868043c62
Kazunori Kurata <kazu7610nori@s.okayama-u.ac.jp>
2023-02-11 01:09:39 +0900

    Serverspecでサーバの起動状態をテストする

---
+++ spec/TargetNode/sample_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe service('nginx') do
+    it { should be_running }
+end
+
+describe command("ps aux | grep unicorn" ) do
+    its(:stdout) { should contain("unicorn_rails master") }
+end
+
+describe port(80) do
+    it { should be_listening }
+end
