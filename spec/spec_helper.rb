b95c8fa3e713020629057c0438799434072eca33
Kazunori Kurata <kazu7610nori@s.okayama-u.ac.jp>
2023-02-11 15:45:01 +0900

    host名の編集。環境変数と鍵の登録

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,8 @@ else
   set :sudo_password, ENV['SUDO_PASSWORD']
 end
 
-host = ENV['TARGET_HOST']
+
+host = TARGETHOST
 
 options = Net::SSH::Config.for(host)
 
@@ -24,7 +25,7 @@ set :host,        options[:host_name] || host
 set :ssh_options, options
 
 # Disable sudo
-# set :disable_sudo, true
+set :disable_sudo, true
 
 
 # Set environment variables
