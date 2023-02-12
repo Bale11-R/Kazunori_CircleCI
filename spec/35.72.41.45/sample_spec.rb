2985ebd4fd0a02ebb9ed7403de64511b24789653
Kazunori Kurata <kazu7610nori@s.okayama-u.ac.jp>
2023-02-12 19:22:53 +0900

    serverspec:port確認テストの削除

--- spec/35.72.41.45/sample_spec.rb
+++ spec/35.72.41.45/sample_spec.rb
@@ -14,10 +14,6 @@ describe command("ps aux | grep unicorn" ) do
     its(:stdout) { should contain("unicorn master") }
 end
 
-describe port(listen_port) do
-    it { should be_listening }
-end
-
 describe command('curl http://kurata-alb-02-190809292.ap-northeast-1.elb.amazonaws.com/ -o /dev/null -w "%{http_code}\n" -s') do
   its(:stdout) { should match /^200$/ }
 end
