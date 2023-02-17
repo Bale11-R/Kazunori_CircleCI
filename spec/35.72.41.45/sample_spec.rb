486227f3130604f9b9481ed8eb9738cd678a1742
Kazunori Kurata <kazu7610nori@s.okayama-u.ac.jp>
2023-02-17 20:47:16 +0900

    .gitignore廃止、DNS編集

--- spec/35.72.41.45/sample_spec.rb
+++ spec/35.72.41.45/sample_spec.rb
@@ -14,6 +14,6 @@ describe command("ps aux | grep unicorn" ) do
     its(:stdout) { should contain("unicorn master") }
 end
 
-describe command('curl http://kurata-alb-02-190809292.ap-northeast-1.elb.amazonaws.com/ -o /dev/null -w "%{http_code}\n" -s') do
+describe command('curl http://kurata-alb-02-255736633.ap-northeast-1.elb.amazonaws.com/ -o /dev/null -w "%{http_code}\n" -s') do
   its(:stdout) { should match /^200$/ }
 end
